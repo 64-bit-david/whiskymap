90a720a1af4f2d03b040db374be6a3ee7a51e8c3
David Williamson <dwilliamsonwebdev@gmail.com>
2021-04-20 17:21:46 +0100

    added abilty for user to sroll through search results with arrow keys

--- src/components/Drop-Search.js
+++ src/components/Drop-Search.js
@@ -1,8 +1,8 @@
-import React, { useState, createRef, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect, useRef } from 'react';
 import { FlyToInterpolator } from 'react-map-gl';
 import * as d3 from 'd3-ease';
+
+
 
 const DropSearch = ({
   distilleryList,
@@ -14,8 +14,13 @@ const DropSearch = ({
 }) => {
 
   const [distilleriesToShow, setDistilleriesToShow] = useState([]);
+  const [focusedDistillery, setFocusedDistillery] = useState(null);
   const [userInput, setUserInput] = useState('');
+  const [focused, setFocused] = useState(false);
+  const [keyScroll, setKeyScroll] = useState(false);
 
+
+  const distilleryListSearch = useRef();
 
 
 
@@ -33,6 +38,7 @@ const DropSearch = ({
   }
 
 
+
   //as user types in searchbar, creates an array of distilleries where name matches user input
   useEffect(() => {
     if (userInput.length > 0) {
@@ -41,16 +47,64 @@ const DropSearch = ({
       });
       setDistilleriesToShow(results);
     }
-  }, [userInput])
+    if (userInput.length === 0) {
+      setDistilleriesToShow([]);
+      setFocusedDistillery(-1);
+    }
+  }, [userInput, distilleryList, setFocusedDistillery]);
 
 
-  //add an onclick here that somehow selected the distillery, sets selectedDistillery as well
+  useEffect(() => {
+    const searchListener = (e) => {
+      if (distilleriesToShow.length > 0) {
+        if (e.key === 'ArrowDown') {
+          setKeyScroll(true);
+          if (focusedDistillery < distilleriesToShow.length - 1 && focusedDistillery < 5 && focused > 0) {
+            setFocusedDistillery(focusedDistillery + 1);
+            // distilleryListSearch.current.children[focusedDistillery].children[0].focus()
+          }
+        }
+        else if (e.key === 'ArrowUp') {
+          setKeyScroll(true);
+          if (focusedDistillery >= 1) {
+            setFocusedDistillery(focusedDistillery - 1);
+            // distilleryListSearch.current.children[focusedDistillery].children[0].focus()
+
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', searchListener);
+    return () => {
+      window.removeEventListener('keydown', searchListener);
+      setKeyScroll(false)
+    }
+  }, [distilleriesToShow.length, focusedDistillery, focused]);
+
+
+
+  useEffect(() => {
+    if (distilleriesToShow.length > 0 && keyScroll) {
+      if (focusedDistillery >= 0) {
+        distilleryListSearch.current.children[focusedDistillery].children[0].focus()
+      }
+      if (focusedDistillery < 0) {
+        distilleryListSearch.current.children[0].children[0].blur();
+
+      }
+    }
+  }, [distilleriesToShow, focusedDistillery, keyScroll])
+
+
+
+
 
   const searchList = () => {
     if (userInput.length > 0) {
+
       return (
-        <ul className={`${navState && 'clicked'}`}>
-          {distilleriesToShow.slice(0, 6).map(distillery => {
+        <ul className={`${focused && 'clicked'}`} ref={distilleryListSearch}>
+          {distilleriesToShow.slice(0, 6).map((distillery, index) => {
             return (
               <li key={distillery.id}>
                 <button
@@ -73,8 +127,8 @@ const DropSearch = ({
           })}
         </ul>
       )
-    }
 
+    }
   }
 
   const searchInput = () => {
@@ -84,7 +138,11 @@ const DropSearch = ({
           aria-label="Search for a distillery"
           placeholder="Search for a distillery..."
           onChange={(e) => setUserInput(e.target.value)}
-          value={userInput} />
+          value={userInput}
+          onFocus={() => setFocused(true)}
+          onBlur={() => {
+            setFocusedDistillery(0);
+          }} />
         {searchList()}
       </div>
     )
